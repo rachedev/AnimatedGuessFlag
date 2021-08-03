445025428f6cdfc7699eefe4371a6f0801b4cedd
Rachelle Lee <rachedeveloper@gmail.com>
2021-08-03 15:03:06 -0700

    Initial Commit

---
+++ AnimatedGuessFlag/AnimatedGuessFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedGuessFlagApp.swift
+//  AnimatedGuessFlag
+//
+//  Created by RachelleLee on 8/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedGuessFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
